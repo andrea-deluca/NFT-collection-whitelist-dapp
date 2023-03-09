f70b0be8b4b46b2aa6fcfc29c266fc229e06b486
Andrea Deluca <79919899+andrea-deluca@users.noreply.github.com>
2023-03-09 22:19:50 +0100

    Whitelist dapp development

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({path: ".env"});
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.18",
+    networks: {
+        goerli: {
+            url: process.env.QUICKNODE_HTTP_URL,
+            accounts: [process.env.PRIVATE_KEY]
+        }
+    }
+};
